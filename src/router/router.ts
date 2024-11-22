1b6d46f0a478b9619138355f0008ffcd47e73ab7
Nexusia <164480547@qq.com>
2024-11-22 17:52:42 +0800

    test 4

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,4 +1,12 @@
-import { createRouter,createWebHistory } from "vue-router"
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Ricardo_Mars
+ * @Date: 2024-11-20 15:31:06
+ * @LastEditors: Ricardo_Mars
+ * @LastEditTime: 2024-11-22 17:51:54
+ */
+import { createRouter,createWebHashHistory } from "vue-router"
 import homePage from "../components/indexPage.vue"
 import testPage1 from "../components/testPage.vue"
 
@@ -16,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history:createWebHistory(),
+    history:createWebHashHistory(),
     routes
 })
 
